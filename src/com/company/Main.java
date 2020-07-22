2d03495825ce23cec06a0ab61f18017453c18a99
Snigdha Sitaula <snigdhasitaula@Snigdhas-MBP.lan>
2020-07-22 12:17:48 -0500

    added current date and time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Snigdha Sitaula :)");
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println(formatter.format(date));
+
         //System.out.println("Java Automation\n");
 
         /*
